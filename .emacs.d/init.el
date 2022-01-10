51053a70352d305ea59b5f59845e2ae7cdc6f6fd
larssve <sveen@protonmail.ch>
2022-01-10 18:22:51 +0100

    Update packages

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -4,7 +4,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("bb08c73af94ee74453c90422485b29e5643b73b05e8de029a6909af6a3fb3f58" default))
+   '("028c226411a386abc7f7a0fba1a2ebfae5fe69e2a816f54898df41a6a3412bb5" "fe2539ccf78f28c519541e37dc77115c6c7c2efcec18b970b16e4a4d2cd9891d" "06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" "bb08c73af94ee74453c90422485b29e5643b73b05e8de029a6909af6a3fb3f58" default))
  '(package-selected-packages '(org-bullets magit vertico evil-escape evil)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
@@ -18,7 +18,7 @@
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
 (blink-cursor-mode 0)
-(setq-default mode-line-format nil)
+;(setq-default mode-line-format nil)
 
 ;; buffer spacing
 ;(setq line-spacing 0.1) 
@@ -29,16 +29,17 @@
 (add-to-list 'package-archives
 	     '("melpa" . "https://melpa.org/packages/"))
 (package-initialize)
-;(package-refresh-contents) 
+;(package-refresh-contents)
 
 ;; add and install packages
-(defvar my-packages '(color-theme-sanityinc-tomorrow
-		      clojure-mode
+(defvar my-packages '(clojure-mode
+		      doom-themes
 		      inf-clojure
 		      geiser-chicken
 		      evil
 		      evil-escape
 		      magit
+		      neotree
 		      org-bullets
 		      paredit
 		      undo-tree
@@ -66,11 +67,21 @@
 (setq-default evil-escape-key-sequence "jk")
 (evil-escape-mode)
 
+(require 'neotree)
+(global-set-key [f2] 'neotree-toggle)
+(setq neo-theme 'arrows)
+
+(require 'doom-themes)
+(setq doom-themes-enable-bold t
+      doom-themes-enable-italic t)
+
 ;;; org mode
 (require 'org)
 (require 'org-bullets)
 (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1)))
 
+
+;(doom-themes-org-config)
 (setq org-startup-indented t
       org-bullets-bullet-list '(" ") ;; no bullets, needs org-bullets package
       org-ellipsis " ..." ;; folding symbol
@@ -83,9 +94,8 @@
       org-fontify-quote-and-verse-blocks t)
 
 ;; fonts
-(set-face-attribute 'default nil :font "Cascadia Code" :height 120)
+;(set-face-attribute 'default nil :font "Cascadia Code" :height 120)
+(set-face-attribute 'default nil :font "IBM Plex Mono" :height 120)
 
 ;; theme
-(require 'color-theme-sanityinc-tomorrow)
-(load-theme 'sanityinc-tomorrow-day)
-
+(load-theme 'doom-tomorrow-day t)
